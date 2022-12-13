a0a3a6dc46011584860dd4b9eaccaa4d471fa8d5
yash-bhootda <yrbhootda@gmail.com>
2022-12-13 21:12:26 +0530

    first

---
+++ Captcha_Recognition/src/config.py
@@ -0,0 +1,9 @@
+# DATA_DIR = "../input/captcha_images_v2"
+DATA_DIR = "D:/SEMESTER5/ISA/JCOMP/captcha-recognition-pytorch-master/captcha-recognition-pytorch-master/input/captcha_images_v2"
+BATCH_SIZE = 8
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 75
+NUM_WORKERS = 8
+EPOCHS = 200
+DEVICE = "cpu"
+# D:\SEMESTER5\ISA\JCOMP\captcha-recognition-pytorch-master\captcha-recognition-pytorch-master\input\captcha_images_v2
